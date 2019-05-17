165a05bd917ea0ec84ca2d249aac3c6041ad534f
Honza Bittner <git@honzabittner.cz>
2019-05-17 11:20:15 +0200

    Finish homework

---
+++ Assets/Scripts/MovableWall.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableWall : MonoBehaviour
+{
+    float offset;
+
+    float time;
+    public float interval;
+    
+    bool isOpen;
+    public int direction;
+    public bool reverse;
+
+    void Start()
+    {
+        Collider _collider = GetComponent<Collider>();
+        offset = _collider.bounds.size.x + _collider.bounds.size.z;
+
+        time = 0;
+        isOpen = false;
+        reverse = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > interval)
+        {
+            time = 0;
+            isOpen = !isOpen;
+
+            float _offset = offset;
+
+            if (reverse) {
+                _offset = -_offset;
+            }
+
+            if (isOpen) {
+                if (direction == 1) {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);
+                } else if (direction == 2) {
+                    transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
+                }
+                
+            } else {
+                if (direction == 1) {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
+                } else if (direction == 2) {
+                    transform.position = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z);
+                }
+                
+            }
+        }
+
+        time += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/gate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gate : MonoBehaviour
+{
+    bool isOpen;
+    private Vector3 startingPosition;
+    public int requiredCount;
+    HashSet<GameObject> activatedButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        startingPosition = transform.position;
+        activatedButtons = new HashSet<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle(GameObject button) {
+        activatedButtons.Add(button);
+
+        if (activatedButtons.Count < requiredCount) {
+            return;
+        }
+
+        isOpen = !isOpen;
+
+        if (!isOpen) {
+            transform.position = startingPosition;
+        } else {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
+        }        
+    }
+}
